c03c34c6e3710a98a7b5d6d38455729179042c85
cphelps76 <cphelps76@gmail.com>
2013-09-30 21:01:54 -0500

    the start of the Gummy java classes

--- src/com/android/settings/gummy/superuser/PolicyNativeFragment.java
+++ src/com/android/settings/gummy/superuser/PolicyNativeFragment.java
@@ -1,4 +1,4 @@
-package com.android.settings.cyanogenmod.superuser;
+package com.android.settings.gummy.superuser;
 
 public class PolicyNativeFragment extends com.koushikdutta.superuser.PolicyNativeFragment {
 }
