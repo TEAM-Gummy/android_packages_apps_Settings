c8e76fb42a1fdf4d208dda0a8cef4c5dcd57a157
David L.-W <david.leppla.weber@gmail.com>
2013-12-31 14:20:13 +0100

    [2/2] Settings: LockScreen Notifications by AOSPAL
    THANKS to AOSPAL -> David L.-W <david.leppla.weber@gmail.com>
    
    Port from : http://review.carbon-rom.com/#/c/1398/
    
    - Enable/disable lockscreen notifs
    - Pocket mode
    - Show always
    - Hide Low Priority
    - Hide Non Clearable
    - Allow dismissing non clearable notifications
    - Eenable/disable expanded notifications
    - Force expanded view
    
    Lockscreen Notifications: wake on notification
    
    Lockscreen Notifications: make notification count user configurable
    
    Lockscreen Notifications: disable notification count when notfifs are disabled
    
    Lockscreen Notifications: check for proximity sensor
    
    PS3: New Options (privacy & position)
    
    Lockscreen Notifications: make offset top user configurable & add privacy mode [2/2]
    
    PS4: fix derp, whitespace
    PS5: code style changes
    PS6: Off by default
    
    Conflicts:
    	res/values/dimens.xml
    	res/values/strings.xml
    	res/xml/system_ui_settings.xml
    
    Change-Id: I6a7ce3370c515aba7e38c83e32c5f28cf0dd2898

---
+++ src/com/android/settings/gummy/preference/NumberPickerPreference.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2013 Gummy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+
+ * Based on: http://www.lukehorvat.com/blog/android-numberpickerdialogpreference/
+ * Thanks to the original author!
+ */
+
+package com.android.settings.gummy.preference;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import com.android.settings.R;
+/**
+ * A {@link DialogPreference} that provides a user with the means to select an integer from a {@linkNumberPicker}, and persist it.
+ *
+ * @author lukehorvat
+ *
+ */
+    public class NumberPickerPreference extends DialogPreference
+    {
+    private static final int DEFAULT_MIN_VALUE = 0;
+    private static final int DEFAULT_MAX_VALUE = 100;
+    private static final int DEFAULT_VALUE = 0;
+
+    private int mMinValue;
+    private int mMaxValue;
+    private int mValue;
+    private NumberPicker mNumberPicker;
+
+    public NumberPickerPreference(Context context) {
+        this(context, null);
+    }
+
+    public NumberPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setMinValue(DEFAULT_MIN_VALUE);
+        setMaxValue(DEFAULT_MAX_VALUE);
+
+        // set layout
+        setDialogLayoutResource(R.layout.preference_number_picker_dialog);
+        setPositiveButtonText(android.R.string.ok);
+        setNegativeButtonText(android.R.string.cancel);
+        setDialogIcon(null);
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restore, Object defaultValue) {
+        setValue(restore ? getPersistedInt(DEFAULT_VALUE) : (Integer) defaultValue);
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        return a.getInt(index, DEFAULT_VALUE);
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+
+        mNumberPicker = (NumberPicker) view.findViewById(R.id.number_picker);
+        mNumberPicker.setMinValue(mMinValue);
+        mNumberPicker.setMaxValue(mMaxValue);
+        mNumberPicker.setValue(mValue);
+    }
+
+    public int getMinValue() {
+        return mMinValue;
+    }
+
+    public void setMinValue(int minValue) {
+        mMinValue = minValue;
+        setValue(Math.max(mValue, mMinValue));
+    }
+
+    public int getMaxValue() {
+        return mMaxValue;
+    }
+
+    public void setMaxValue(int maxValue) {
+        mMaxValue = maxValue;
+        setValue(Math.min(mValue, mMaxValue));
+    }
+
+    public int getValue() {
+        return mValue;
+    }
+
+    public void setValue(int value) {
+        value = Math.max(Math.min(value, mMaxValue), mMinValue);
+
+        if (value != mValue) {
+            mValue = value;
+            persistInt(value);
+            notifyChanged();
+        }
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+
+        // when the user selects "OK", persist the new value
+        if (positiveResult) {
+            int numberPickerValue = mNumberPicker.getValue();
+            if (callChangeListener(numberPickerValue)) {
+                setValue(numberPickerValue);
+            }
+        }
+    }
+}
