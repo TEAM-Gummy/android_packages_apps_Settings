38d0b91ea4555f74e02ccf692cbfb07188e2c985
Lars Greiss <kufikugel@googlemail.com>
2014-02-07 13:28:10 +0100

    Settings: SlimPie (2/2)
    Well first I want to thanks again for the ideas Jens Doll, Kyrylo Mikos and PA gave us
    on 4.3 allready to make our own SlimPie possible. We used partly code from them and added
    and changed it in our Slim way to have it for us most usefull.
    
    Well as known SlimPie advantage to other PIEs is that it can bind on every button
    press and longpress action like the user want. All SlimActions are available for
    every button (so all possible shortcuts we have + apps etc). Of course as well
    available are custom icon images or system icons. Other unique thing of SlimPie
    is that 2 rows are available. First row user can setup up to 5 buttons. On the second
    row up to 7 Buttons.
    Of course the user can setup the style to their like and still theme engine related
    things are respected.
    Pie is activated present on non expanded Desktop as well
    
    For 4.4 we reworked a lot of the frameworks base code mainly. Changes compared to 4.3
    
    - We use now a edge detection service (thanks a ton to Jens and Kyrylo). We tweaked it only
      very minor due that it works allready out of the box. Two reasons why we want to use it.
    
      1. We do not have detection overlays on the borders anymore. Means the user is not disturbed
         by it and we do not need to add workarounds like we did before to avoid accidental swipe ups
         (eg old IME reduce triggers). As well it is not needed anymore that the user can setup
         thikness etc. The default values are allready quiet good. If a device has detection problems
         the dm can adjust them via overlay. As said main main main advantage  that no dead zones are present
         anymore.
    
      2. we will use this service for other upcomming features as well (eg new Recents)
    
    - Centralized and adapted the code to 4.4 and some changes we did in our central classes
    - reduced observer to an absolutly minimum. If PIE is disabled it does not observe any value
    - A lot small fixes and tweaks
    - Change default colors to KK style
    - If navbar is present the navbar gets animated with the background to give a full screen overlay
      feeling to the user. Especially now with kk transparency way better.
    - Full keyguard support
    - complete reworked edge disable detection
    - and a lot more
    
    PS:
    - better disable message for second layer
    PS:
    - fix wrong initialize from pie control settings
    PS
    - changed to Jubas suggestions
    
    Change-Id: Ia861e06758a6dadcb5a6b52689e574308b2dcc02

--- src/com/android/settings/gummy/dslv/ButtonsListViewSettings.java
+++ src/com/android/settings/gummy/dslv/ButtonsListViewSettings.java
@@ -469,6 +469,12 @@ public class ButtonsListViewSettings extends ListFragment implements
             case NAV_RING:
                 return ButtonsHelper.getNavRingConfigWithDescription(
                     mActivity, mActionValuesKey, mActionEntriesKey);
+            case PIE:
+                return ButtonsHelper.getPieConfigWithDescription(
+                    mActivity, mActionValuesKey, mActionEntriesKey);
+            case PIE_SECOND:
+                return ButtonsHelper.getPieSecondLayerConfigWithDescription(
+                    mActivity, mActionValuesKey, mActionEntriesKey);
             case POWER_MENU_SHORTCUT:
                 return PolicyHelper.getPowerMenuConfigWithDescription(
                     mActivity, mActionValuesKey, mActionEntriesKey);
@@ -485,6 +491,12 @@ public class ButtonsListViewSettings extends ListFragment implements
                 break;
             case NAV_RING:
                 ButtonsHelper.setNavRingConfig(mActivity, buttonConfigs, reset);
+                break;
+            case PIE:
+                ButtonsHelper.setPieConfig(mActivity, buttonConfigs, reset);
+                break;
+            case PIE_SECOND:
+                ButtonsHelper.setPieSecondLayerConfig(mActivity, buttonConfigs, reset);
                 break;
             case POWER_MENU_SHORTCUT:
                 PolicyHelper.setPowerMenuConfig(mActivity, buttonConfigs, reset);
@@ -643,8 +655,8 @@ public class ButtonsListViewSettings extends ListFragment implements
                             break;
                         case NAV_BAR:
                         case NAV_RING:
-                        // case PIE:
-                        // case PIE_SECOND:
+                        case PIE:
+                        case PIE_SECOND:
                         default:
                             buttonMode = res.getString(R.string.shortcut_action_help_button);
                             break;
@@ -656,7 +668,9 @@ public class ButtonsListViewSettings extends ListFragment implements
                         R.string.shortcut_action_help_main, buttonMode, icon);
                     if (!getOwner().mDisableDeleteLastEntry) {
                         finalHelpMessage += " " + res.getString(
-                            R.string.shortcut_action_help_delete_last_entry, buttonMode);
+                                getOwner().mButtonMode == PIE_SECOND
+                                ? R.string.shortcut_action_help_pie_second_layer_delete_last_entry
+                                : R.string.shortcut_action_help_delete_last_entry, buttonMode);
                     }
                     return new AlertDialog.Builder(getActivity())
                     .setTitle(R.string.shortcut_action_help)
