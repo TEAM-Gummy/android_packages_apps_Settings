6552f0daf6d6cea1fc254310c74b9b82320ee920
cphelps76 <cphelps76@gmail.com>
2014-04-22 23:05:42 -0500

    QuickBoot: enable QuickBoot Settings UI Add QuickBoot enable item in AdvancedOptions.
    Change-Id: I238cc2e74bd2ec3c9bfc6a1f6fef2de137d12cb4

--- src/com/android/settings/gummy/interfaceClasses/AdvancedOptions.java
+++ src/com/android/settings/gummy/interfaceClasses/AdvancedOptions.java
@@ -17,11 +17,16 @@
 package com.android.settings.gummy.interfaceClasses;
 
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.PreferenceCategory;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -33,9 +38,12 @@ public class AdvancedOptions extends SettingsPreferenceFragment implements
 
     private static final String KEY_HARDWARE_KEYS = "hardware_keys";
     private static final String KEY_ADVANCED_OPTIONS = "advanced_options";
+    private static final String ENABLE_QUICKBOOT_KEY = "enable_quickboot";
+    private static final String QUICKBOOT_PACKAGE_NAME = "com.qapp.quickboot";
 
     private PreferenceScreen mHardwareKeys;
     private PreferenceCategory mAdvancedOptions;
+    private CheckBoxPreference mQuickBoot;
 
     public boolean hasButtons() {
         return !getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
@@ -52,11 +60,15 @@ public class AdvancedOptions extends SettingsPreferenceFragment implements
         // Only show the hardware keys config on a device that does not have a navbar
         mAdvancedOptions = (PreferenceCategory) prefSet.findPreference(KEY_ADVANCED_OPTIONS);
         mHardwareKeys = (PreferenceScreen) findPreference(KEY_HARDWARE_KEYS);
-
         if (!hasButtons()) {
             mAdvancedOptions.removePreference(mHardwareKeys);
         }
 
+        // Quickboot
+        mQuickBoot = findAndInitCheckboxPref(ENABLE_QUICKBOOT_KEY);
+        if (!isPackageInstalled(QUICKBOOT_PACKAGE_NAME)) {
+            removePreference(mQuickBoot);
+        }
     }
 
         @Override
@@ -71,11 +83,38 @@ public class AdvancedOptions extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
+        if (preference == mQuickBoot) {
+            Settings.Global.putInt(getActivity().getContentResolver(),
+                    Settings.Global.ENABLE_QUICKBOOT,
+                    mQuickBoot.isChecked() ? 1 : 0);
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+        return false;
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
         return true;
+    }
+
+    private static boolean isPackageInstalled(Context context, String packageName) {
+        try {
+            return context.getPackageManager().getPackageInfo(packageName, 0) != null;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    private CheckBoxPreference findAndInitCheckboxPref(String key) {
+        CheckBoxPreference pref = (CheckBoxPreference) findPreference(key);
+        if (pref == null) {
+            throw new IllegalArgumentException("Cannot find preference with key = " + key);
+        }
+        return pref;
+    }
+
+    private void removePreference(Preference preference) {
+        getPreferenceScreen().removePreference(preference);
     }
 }
