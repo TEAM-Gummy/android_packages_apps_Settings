e1069ffbf9d6bec0d081c341573560fab350322d
dankoman <dankoman30@gmail.com>
2014-08-15 21:07:14 +0200

    Settings: Volume panel background color && transparency [1/2]
    Allow user to specify background color and transparency level
        for volume panel.
    
    Conflicts:
    	res/values-de/slim_strings.xml
    	res/values-fr/slim_strings.xml
    	res/values-it/slim_strings.xml
    	res/values/gummy_strings.xml
    	res/xml/sound_settings.xml
    	src/com/android/settings/SoundSettings.java
    	src/com/android/settings/accessibility/AccessibilitySettings.java
    
    Change-Id: Ic788b4ccf1826075a600e53bc6a7c3c8bda50685

--- src/com/android/settings/gummy/interfaceClasses/AdvancedOptions.java
+++ src/com/android/settings/gummy/interfaceClasses/AdvancedOptions.java
@@ -16,8 +16,10 @@
 
 package com.android.settings.gummy.interfaceClasses;
 
+import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
@@ -27,6 +29,12 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.view.VolumePanel;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -40,10 +48,15 @@ public class AdvancedOptions extends SettingsPreferenceFragment implements
     private static final String KEY_ADVANCED_OPTIONS = "advanced_options";
     private static final String ENABLE_QUICKBOOT_KEY = "enable_quickboot";
     private static final String QUICKBOOT_PACKAGE_NAME = "com.qapp.quickboot";
+    private static final String VOLUME_PANEL_BG_COLOR = "volume_panel_bg_color";
+
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DEFAULT_BACKGROUND_COLOR = 0x00ffffff;
 
     private PreferenceScreen mHardwareKeys;
     private PreferenceCategory mAdvancedOptions;
     private CheckBoxPreference mQuickBoot;
+    private ColorPickerPreference mVolumePanelBgColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,7 +76,60 @@ public class AdvancedOptions extends SettingsPreferenceFragment implements
             mAdvancedOptions.removePreference(mQuickBoot);
         }
 
+        // Volume panel background color
+        mVolumePanelBgColor =
+                (ColorPickerPreference) findPreference(VOLUME_PANEL_BG_COLOR);
+        mVolumePanelBgColor.setOnPreferenceChangeListener(this);
+        final int intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.VOLUME_PANEL_BG_COLOR, 0x00ffffff);
+        String hexColor = String.format("#%08x", (0x00ffffff & intColor));
+        if (hexColor.equals("#00ffffff")) {
+            mVolumePanelBgColor.setSummary(R.string.default_color);
+        } else {
+            mVolumePanelBgColor.setSummary(hexColor);
+        }
+        mVolumePanelBgColor.setNewPreviewColor(intColor);
+        setHasOptionsMenu(true);
+
         removeHardwareKeyPreference();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset_default_message)
+                .setIcon(R.drawable.ic_settings_backup)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                resetToDefault();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void resetToDefault() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+        alertDialog.setTitle(R.string.shortcut_action_reset);
+        alertDialog.setMessage(R.string.qs_style_reset_message);
+        alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                resetValues();
+            }
+        });
+        alertDialog.setNegativeButton(R.string.cancel, null);
+        alertDialog.create().show();
+    }
+
+    private void resetValues() {
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.VOLUME_PANEL_BG_COLOR, DEFAULT_BACKGROUND_COLOR);
+        mVolumePanelBgColor.setNewPreviewColor(DEFAULT_BACKGROUND_COLOR);
+        mVolumePanelBgColor.setSummary(R.string.default_color);
     }
 
         @Override
@@ -91,7 +157,20 @@ public class AdvancedOptions extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        final String key = preference.getKey();
+        if (preference == mVolumePanelBgColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(objValue)));
+            if (hex.equals("#00ffffff")) {
+                preference.setSummary(R.string.default_color);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.VOLUME_PANEL_BG_COLOR,
+                    intHex);
+            return true;
+        }
         return true;
     }
 
